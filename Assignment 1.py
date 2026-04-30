61f92901a58bc551dbeda07675e925349fd2f3a9
Aakashtyagi9889 <aakashtyagi9889@gmail.com>
2026-04-30 18:15:18 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,131 @@
+"""
+1 : Write a program to print “Hello Python” on the screen.
+         ==> print ("Hello Python ")
+         
+2. Take a name as input and print Welcome <name>
+        ==>
+            name  = input()
+            print("Welcome" , name)
+            
+3. Take a number as input and print it.
+        ==> Num = input()
+            print(Num)
+            
+4. Take two numbers as input and print both numbers on separate lines.
+        ==>num1 = input()
+            num2 = input()
+            print(num1)
+            print(num2)
+5. Take a name and age as input and print: My name is ___ and my age is ___
+        ==>name = input()
+            age = input()
+            print("My name is ",name,"and my age is" ,age)
+            
+6. Take an integer as input and print its square.
+        ==>
+            square  =  int(input())
+            print(square*square)
+
+7. Take two integers as input and print their sum.
+        ==> num1 = int(input())
+            num2 = int(input())
+            print(num1+num2)
+
+8. Take two integers and print: o Addition o Subtraction o Multiplication
+        ==> num1 = int(input())
+            num2 = int(input())
+            addition = num1 + num2
+            print ("Addition : " , addition)
+            subtraction = num1 - num2
+            print("Subtraction : " , subtraction)
+            multiplication = num1 * num2
+            print("Multiplication : ", multiplication)
+            
+9. Take a number and print: The number entered is <number> 
+        ==> num = int(input())
+            print("The number entered is : " , "<",num,">")
+            
+10. Take an integer and print its double.
+        ==> number = int(input())
+            print(number + number)
+
+11. Take a float value as input and print it.
+        ==> num = float(input())
+            print(num)
+
+12. Take two float numbers and print their sum.
+        ==> num1 = float(input())
+            num2 = float(input())
+            print( num1 + num2)
+
+13. Take a float number and print its half value.
+        ==> num = float(input())
+            print(num/2)
+
+14. Take length and width (float) as input and print the area of a rectangle.  (length*width)
+        ==> length = float(input())
+            Width  =  float(input())
+            print("Area of Rectangle : " , length * Width)
+
+15. Take radius as float input and print the area of a circle (Use π = 3.14)
+        ==> radius = float(input())
+            print("Area of Circle : " ,  3.14*radius*radius)
+
+16. Take one integer and one float as input and print both.
+        ==> num1 = int(input())
+            num2 = float(input())
+            print(num1, num2)
+
+17. Take total marks (int) and number of subjects (int) and print the average as float.
+        ==> totalMarks = int(input())
+            Subjects = int(input())
+            average =  float(totalMarks/Subjects)
+            print(average)
+
+18. Take price (float) and quantity (int) and print the total cost.
+        ==> price =  float(input())
+            quantity = int(input())
+            print("Total Cost : ",price*quantity)
+
+19. Take two numbers (one int, one float) and print their sum.
+        ==> num1 = int(input())
+            num2 = float(input())
+            print( num1 + num2)
+
+20. Take salary (float) and bonus (int) and print the final salary.
+        ==> salary =  float(input())
+            bonus = int(input())
+            FinalSalary = salary + bonus
+            print(FinalSalary)
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a = 21
+b = 56
+c
+print(a & b)
+
+
+
+
+
+
+
+
+
+
+
